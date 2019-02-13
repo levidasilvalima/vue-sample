b2f906cdf6373230e2b78da28826ea946bdb301c
Levi da Silva Lima <levidasilvalima.ldsl@gmail.com>
2019-02-12 21:55:36 -0400

    feat: add fail message componet

---
+++ src/components/FailMessage.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-layout justify-center align-center column pa-5>
+        <div class="display-4 font-weight-black white--text text-xs-center">SORRY</div>
+        <div class="display-3 font-weight-black white--text text-xs-center mb-3">Man, you shouldn't be here !!</div>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    name: 'FailMessage'
+};
+</script>
+
+<style scoped>
+.home-hero{
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,27 +1,28 @@
 <template>
-    <v-container v-if="isAuthenticated" fluid fill-height  class="teal darken-1 darken-2 home-hero" style="max-height: 100vh;">
-        <v-layout justify-center align-center column pa-5>
+    <v-container fluid fill-height  class="teal darken-1 darken-2 home-hero" style="max-height: 100vh;">
+        <v-layout v-if="isAuthenticated" justify-center align-center column pa-5>
             <div class="display-4 font-weight-black white--text text-xs-center">WELCOME TO MENU 1</div>
             <div class="display-3 font-weight-black white--text text-xs-center mb-3">This is an presentation of the menu 1</div>
             <div class="display-1 font-weight-bold white--text text-xs-center">Everything in life we should to do.</div>
         </v-layout>
-    </v-container>
-    <v-container v-else fluid fill-height  class="teal darken-1 darken-2 home-hero" style="max-height: 100vh;">
-        <v-layout justify-center align-center column pa-5>
-            <div class="display-4 font-weight-black white--text text-xs-center">SORRY</div>
-            <div class="display-3 font-weight-black white--text text-xs-center mb-3">Man, you shouldn't be here !!</div>
-        </v-layout>
+        <fail-message v-else/>
     </v-container>
 </template>
 
 <script>
 
+import FailMessage from '@/components/FailMessage';
+
 export default {
     name: 'menu 1',
+    components: {
+        FailMessage
+    },
     computed: {
         isAuthenticated() {
             return this.$store.getters.isAuthenticated;
         }
     },
+
 };
 </script>
