8f8512af44b2e343ce2f3b69dbf98662302a8514
williamads <williamsilva.uea@gmail.com>
2019-02-12 21:21:51 -0400

    chore: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app id="app">
     <app-navigation></app-navigation>
-    <!-- <v-content transition="slide-x-transition"> -->
-        <router-view></router-view>
-    <!-- </v-content> -->
-    <!-- <app-footer></app-footer> -->
+      <router-view></router-view>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
 import AppNavigation from '@/components/AppNavigation';
-// import AppFooter from '@/components/AppFooter';
+import AppFooter from '@/components/AppFooter';
 export default {
   name: 'App',
   components: {
-    AppNavigation
-    // AppFooter
+    AppNavigation,
+    AppFooter
   }
 }
 </script>
